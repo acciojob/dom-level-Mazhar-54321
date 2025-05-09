67060cca8ab9f5d4132729956a70826598dd7435
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-09 08:12:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+let el = document.getElementById("level");
+let c=0;
+while(el.parentNode !=null){
+   c++;
+   el = el.parentNode;
+}
+alert(`The level of the element is: ${c}`)
